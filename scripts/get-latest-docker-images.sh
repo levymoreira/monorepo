3ddf2c35a6f794550c6e161cbcf34b03ab69fadc
Levy Moreira <levymoreira.ce@gmail.com>
2025-11-24 19:25:51 +0000

    fff

--- scripts/get-latest-docker-images.sh
+++ scripts/get-latest-docker-images.sh
@@ -14,9 +14,9 @@ BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 
 # Configuration (can be overridden via environment)
-ACR_REGISTRY=${ACR_REGISTRY:-monoreporegistry.azurecr.io}
-ACR_USERNAME=${ACR_USERNAME}
-ACR_PASSWORD=${ACR_PASSWORD}
+ACR_REGISTRY='monoreporegistry.azurecr.io'
+ACR_USERNAME='monoreporegistry'
+ACR_PASSWORD='4LrJVj4WpydE0AbB9LH+TvdfiJx7cGhseEXWpuTbJ0+ACRBwrhom'
 PROJECT_NAME=${PROJECT_NAME:-monorepo}
 ALL_SERVICES=("next-app-one" "next-app-two" "express-api" "cron-logger")
 
